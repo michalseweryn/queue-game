a68382b3bb4644e99d586ae4c33e725216063fa3
phantom10111 <phantom10111@gmail.com>
2013-05-14 23:22:12 +0200

    Dałem testy w dobre miejsce

--- src/test/java/queue_game/model/GameStateTest.java
+++ src/test/java/queue_game/model/GameStateTest.java
@@ -90,4 +90,28 @@ public class GameStateTest {
 		gameState.sell(ProductType.CLOTHES);
 	}
 
+	@Test
+	public void resetNumberOfProductsTest2() {
+		GameState g= new GameState();
+		g.resetNumberOfProducts();
+		Integer tab[]=g.getNumberOfProducts();
+		assertEquals(new Integer(50),tab[2]);
+	}
+
+	@Test
+	public void resetPlayersTest2() {
+		GameState g= new GameState();
+		g.reset(5);
+		g.resetPlayers();
+		assertEquals(5,g.getPlayersList().get(1).getNumberOfPawns());
+	}
+
+	@Test
+	public void putPawnofSpeculatorTest() {
+		GameState g= new GameState();
+		g.reset(5);
+		g.putPawnofSpeculator(ProductType.FURNITURE);
+		assertEquals(true,g.getStore(ProductType.FURNITURE).getQueue().contains(-1));
+	}
+
 }
