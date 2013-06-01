376ddee4776d64c14de8cc5b622dd9a35b294ab0
Helena <Helena@Helena-Komputer>
2013-06-01 17:30:33 +0200

    Usprawnienie kodu menu.

--- src/main/java/queue_game/view/MenuApp.java
+++ src/main/java/queue_game/view/MenuApp.java
@@ -15,7 +15,13 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class MenuApp {
-
+	public Color[] kolory = new Color[]{
+		Color.CYAN, Color.GREEN, Color.PINK, Color.YELLOW
+	};
+	public String[][] imiona = new String[][]{
+			{new String("Adam"),new String("Ela")},{new String("Marek23"),new String("Nikt00")},
+			{new String("Michal45"),new String("Jan33")},{new String("Asia23"),new String("Ewa34")}
+		};
 	public MenuApp() {
 		String str = JOptionPane.showInputDialog(null, "Podaj nick : ", "Nick",
 				1);
@@ -33,59 +39,27 @@ public class MenuApp {
 
 	public void Menu(){
 		JFrame frame = new JFrame("Menu");
-		  JLabel label1 = new JLabel("Stol 1: GRA W TRAKCIE");
-		  JLabel label1a = new JLabel("Adam   Ewa   Ktos");
-		  JLabel label2 = new JLabel("Stol 2: GRA W TRAKCIE");
-		  JLabel label2a = new JLabel("Marek   Ela   Nikt");
-		  JLabel label3 = new JLabel("Stol 3: GRA W TRAKCIE");
-		  JLabel label3a = new JLabel("Adam   Ewa   Janek");
-		  JLabel label4 = new JLabel("Stol 4: GRA W TRAKCIE");
-		  JLabel label4a = new JLabel("Michał   Janek");
-		  JButton button1 = new JButton("Dołącz");
-		  JButton button2 = new JButton("Dołącz");
-		  JButton button3 = new JButton("Dołącz");
-		  JButton button4 = new JButton("Dołącz");
-		  JPanel panel1 = new JPanel();
-		  JPanel panel1a = new JPanel();
-		  JPanel panel2 = new JPanel();
-		  JPanel panel2a = new JPanel();
-		  JPanel panel3 = new JPanel();
-		  JPanel panel3a = new JPanel();
-		  JPanel panel4 = new JPanel();
-		  JPanel panel4a = new JPanel();
-		  coloreLabel(label1,Color.CYAN);
-		  coloreLabel(label1a,Color.WHITE);
-		  label1a.setPreferredSize(new Dimension(350,50));
-		  coloreLabel(label2,Color.GREEN);
-		  coloreLabel(label2a,Color.WHITE);
-		  label2a.setPreferredSize(new Dimension(350,50));
-		  coloreLabel(label3,Color.PINK);
-		  coloreLabel(label3a,Color.WHITE);
-		  label3a.setPreferredSize(new Dimension(350,50));
-		  coloreLabel(label4,Color.YELLOW);
-		  coloreLabel(label4a,Color.WHITE);
-		  label4a.setPreferredSize(new Dimension(350,50));
-		  panel1.add(label1);
-		  panel1.add(button1);
-		  panel1a.add(label1a);
-		  panel2.add(label2);
-		  panel2.add(button2);
-		  panel2a.add(label2a);
-		  panel3.add(label3);
-		  panel3.add(button3);
-		  panel3a.add(label3a);
-		  panel4.add(label4);
-		  panel4.add(button4);
-		  panel4a.add(label4a);
-		  frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-		  frame.getContentPane().add(panel1);
-		  frame.getContentPane().add(panel1a);
-		  frame.getContentPane().add(panel2);
-		  frame.getContentPane().add(panel2a);
-		  frame.getContentPane().add(panel3);
-		  frame.getContentPane().add(panel3a);
-		  frame.getContentPane().add(panel4);
-		  frame.getContentPane().add(panel4a);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		for(int i=1;i<5;i++){
+			StringBuilder s = new StringBuilder();
+			for(String j : imiona[i-1]){
+				s.append(j);
+				s.append(" ");
+			}
+			JLabel label = new JLabel("Stol "+i+":");
+			  JLabel labela = new JLabel(s.toString());
+			  JButton button = new JButton("Dołącz");
+			  JPanel panel = new JPanel();
+			  JPanel panela = new JPanel();
+			  coloreLabel(label,kolory[i-1]);
+			  coloreLabel(labela,Color.WHITE);
+			  labela.setPreferredSize(new Dimension(350,50));
+			  panel.add(label);
+			  panel.add(button);
+			  panela.add(labela);
+			  frame.getContentPane().add(panel);
+			  frame.getContentPane().add(panela);
+		}
 		  frame.setSize(400, 450);
 		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		  frame.setVisible(true);
